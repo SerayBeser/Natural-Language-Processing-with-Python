c365f2e6b71632b9a02bc24ea1fdc57db62284a4
SerayBeser <besers56@gmail.com>
2017-11-14 11:13:52 +0300

    getting started

---
+++ ch 01 : Language Processing and Python/00_getting_started.py
@@ -0,0 +1,39 @@
+""" @SerayBeser
+
+Natural Language Processing with Python
+# http://www.nltk.org/book/ch01.html
+
+Python ile Dogal Dil Isleme
+
+"""
+
+# Installing NLTK
+# NLTK 'nin Kurulmasi
+# import nltk
+# nltk.download()
+
+# Load some items(books)
+# Kitaplari Indirelim.
+
+from nltk.book import *
+
+"""
+*** Introductory Examples for the NLTK Book ***
+Loading text1, ..., text9 and sent1, ..., sent9
+Type the name of the text or sentence to view it.
+Type: 'texts()' or 'sents()' to list the materials.
+text1: Moby Dick by Herman Melville 1851
+text2: Sense and Sensibility by Jane Austen 1811
+text3: The Book of Genesis
+text4: Inaugural Address Corpus
+text5: Chat Corpus
+text6: Monty Python and the Holy Grail
+text7: Wall Street Journal
+text8: Personals Corpus
+text9: The Man Who Was Thursday by G . K . Chesterton 1908
+"""
+
+print text2
+
+# Now, we are ready to get started.
+# Elimizde veriler olduguna gore artik baslamaya haziriz.

084c38c3f03f8e92dda7a72b853fa5d05d53eb36
SerayBeser <besers56@gmail.com>
2017-11-16 11:39:09 +0300

    spam filtering

---
+++ course 02 : Spam Filtering/spam_filtering.py
@@ -0,0 +1,11 @@
+""" @SerayBeser
+
+Natural Language Processing with Python
+
+Python ile Dogal Dil Isleme
+
+**** Spam Filtering
+     Spam Filtreleme ****
+
+dataset : http://www2.aueb.gr/users/ion/data/enron-spam/ : Enron 1
+"""
